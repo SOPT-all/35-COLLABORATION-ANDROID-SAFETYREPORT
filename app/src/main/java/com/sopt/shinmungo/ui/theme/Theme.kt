48fb42a83334ecf053f8e1baa7d0ffc70ceac5a2
boiledegg <tjrwns2163@naver.com>
2024-11-18 23:22:52 +0900

    [ADD/#6] 테마 적용 방식 수정

--- app/src/main/java/com/sopt/shinmungo/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/shinmungo/ui/theme/Theme.kt
@@ -1,58 +1,54 @@
 package com.sopt.shinmungo.ui.theme
 
 import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+object ShinMunGoTheme {
+        val color: ShinMunGoColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorProvider.current
+}
 
 @Composable
-fun ShinmungoTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideShinMunGoColors(
+    colors: ShinMunGoColors,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    CompositionLocalProvider(
+        LocalColorProvider provides colors,
         content = content
     )
+}
+
+@Composable
+fun ShinMunGoTheme(
+    backgroundColor: Color = Gray1,
+    content: @Composable () -> Unit
+) {
+    ProvideShinMunGoColors(
+        colors = defaultShinMunGoColors
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    statusBarColor = backgroundColor.toArgb()
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = true
+                }
+            }
+        }
+
+        MaterialTheme(
+            content = content
+        )
+    }
 }
