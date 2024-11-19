9e806e97f389b73256eb653ca914661f0adfe4bc
boiledegg <tjrwns2163@naver.com>
2024-11-19 22:13:32 +0900

    [ADD/#11] 화면 관리 예시

--- app/src/main/java/com/sopt/shinmungo/domain/mapper/ExampleMapper.kt
+++ app/src/main/java/com/sopt/shinmungo/domain/mapper/ExampleMapper.kt
@@ -4,6 +4,7 @@ import com.sopt.shinmungo.data.dto.request.ExampleRequest
 import com.sopt.shinmungo.data.dto.response.ExampleResponse
 import com.sopt.shinmungo.domain.entity.Example
 
+/**TODO: 이해를 위한 예시 매퍼 구현 (삭제될 예정)*/
 fun ExampleResponse.toExample(): Example = Example(
     id = id,
     name = name,
