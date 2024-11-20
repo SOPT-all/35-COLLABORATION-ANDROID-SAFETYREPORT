1edccae462bf199b8e9d3b0a9756cdbc82faee6d
boiledegg <tjrwns2163@naver.com>
2024-11-21 03:34:48 +0900

    [FEAT/#13] 네비게이션 시스템 구현

--- app/src/main/java/com/sopt/shinmungo/core/extension/Modifier.kt
+++ app/src/main/java/com/sopt/shinmungo/core/extension/Modifier.kt
@@ -3,9 +3,11 @@ package com.sopt.shinmungo.core.extension
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.unit.dp
 
 
 /** 리플 효과 제거를 위한 Modifier Extension Function */
@@ -16,3 +18,5 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
         onClick()
     }
 }
+
+fun Modifier.showIf(visibility: Boolean) = if (visibility) this else this.size(0.dp)
