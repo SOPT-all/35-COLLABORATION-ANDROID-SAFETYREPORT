4816d0b0291ce14d9075737d94801ed5adbad1eb
jangsjw <wkdwodnjs1999@gmail.com>
2024-11-21 13:37:04 +0900

    [MOD/#14] ShinMunGoTheme.color 적용

--- app/src/main/java/com/sopt/shinmungo/core/designsystem/component/example/CustomDialog.kt
+++ app/src/main/java/com/sopt/shinmungo/core/designsystem/component/example/CustomDialog.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.SpanStyle
@@ -25,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
 
 @Composable
 fun CustomDialog(
@@ -50,7 +50,7 @@ fun CustomDialog(
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(48.dp)
-                        .background(Color(0xFFFF6F00))
+                        .background(ShinMunGoTheme.color.primary)
                         .padding(horizontal = 16.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
