b2208b78d8535905ed0efb45069a909b4b23f623
chrin05 <rinring105@gmail.com>
2024-11-21 15:56:52 +0900

    [ui/#18] 둥근테두리+아이콘 버튼 컴포넌트 구현

---
+++ app/src/main/java/com/sopt/shinmungo/core/designsystem/component/button/RoundCornerIconButton.kt
@@ -0,0 +1,72 @@
+package com.sopt.shinmungo.core.designsystem.component.button
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.shinmungo.R
+import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
+
+@Composable
+fun RoundedCornerIconButton(
+    @DrawableRes icon: Int,
+    isButtonActive: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val borderLineColor =
+        if (isButtonActive) ShinMunGoTheme.color.gray7 else ShinMunGoTheme.color.gray5
+    val iconLineColor =
+        if (isButtonActive) ShinMunGoTheme.color.gray8 else ShinMunGoTheme.color.gray6
+
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(size = 8.dp))
+            .background(ShinMunGoTheme.color.gray1)
+            .border(width = 1.dp, color = borderLineColor, shape = RoundedCornerShape(size = 8.dp))
+            .padding(horizontal = 14.dp, vertical = 7.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(icon),
+            contentDescription = "아이콘버튼",
+            tint = iconLineColor,
+            modifier = Modifier.size(40.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RoundedCornerIconButtonPreview(modifier: Modifier = Modifier) {
+    ShinMunGoTheme {
+        RoundedCornerIconButton(
+            modifier = Modifier,
+            icon = R.drawable.ic_report_camera_24px,
+            isButtonActive = false
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RoundedCornerIconButtonActivePreview(modifier: Modifier = Modifier) {
+    ShinMunGoTheme {
+        RoundedCornerIconButton(
+            modifier = Modifier,
+            icon = R.drawable.ic_report_camera_24px,
+            isButtonActive = true
+        )
+    }
+}
