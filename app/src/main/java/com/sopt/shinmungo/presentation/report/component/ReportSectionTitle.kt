26cc7a93799f89099e04e4fd225aab556ef4a647
chrin05 <rinring105@gmail.com>
2024-11-23 18:59:58 +0900

    [UI/#22] 신고페이지 섹션별 제목 컴포넌트 구현

---
+++ app/src/main/java/com/sopt/shinmungo/presentation/report/component/ReportSectionTitle.kt
@@ -0,0 +1,58 @@
+package com.sopt.shinmungo.presentation.report.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.shinmungo.R
+import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
+import com.sopt.shinmungo.core.extension.noRippleClickable
+
+@Composable
+fun ReportSectionTitle(
+    text: String,
+    onInfoIconClick: () -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Row {
+        Text(
+            text = text,
+            style = ShinMunGoTheme.typography.body4,
+            color = ShinMunGoTheme.color.gray13
+        )
+        Spacer(modifier = Modifier.width(1.dp))
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_report_orange_star),
+            contentDescription = stringResource(R.string.report_orange_star_content_description),
+            tint = Color.Unspecified,
+            modifier = Modifier.padding(vertical = 1.dp)
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_info_circle_16px),
+            contentDescription = stringResource(R.string.report_info_icon_content_description),
+            tint = Color.Unspecified,
+            modifier = Modifier.noRippleClickable { onInfoIconClick() }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ReportSectionTitlePreview(modifier: Modifier = Modifier) {
+    ShinMunGoTheme {
+        ReportSectionTitle(
+            text = "사진",
+        )
+    }
+}
