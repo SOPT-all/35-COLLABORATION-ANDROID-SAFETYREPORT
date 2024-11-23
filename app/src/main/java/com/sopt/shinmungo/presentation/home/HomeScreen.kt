1114d2fde33c47c1c16ef1ef6c5b7bebf7b9875b
boiledegg <tjrwns2163@naver.com>
2024-11-23 23:52:52 +0900

    [UI/#21] 처리 사례 뷰 구현

--- app/src/main/java/com/sopt/shinmungo/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/shinmungo/presentation/home/HomeScreen.kt
@@ -7,56 +7,136 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.shinmungo.R
+import com.sopt.shinmungo.core.designsystem.component.button.RoundedCornerTextButton
 import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
-import com.sopt.shinmungo.presentation.home.component.topbar.HomeTopBar
-import com.sopt.shinmungo.presentation.home.component.HomeMileageTitleGroup
+import com.sopt.shinmungo.presentation.home.component.HomeImageBannerPager
+import com.sopt.shinmungo.presentation.home.component.HomeIssueExamplePager
 import com.sopt.shinmungo.presentation.home.component.HomeMonthReportGroup
 import com.sopt.shinmungo.presentation.home.component.HomeYearReportGroup
+import com.sopt.shinmungo.presentation.home.component.topbar.HomeTopBar
 
 @Composable
 fun HomeScreen(
+    mileage: String = "5,000,000",
+    yearReportCount: String = "25",
+    monthReportCount: String = "7",
+    userName: String = "수민",
     modifier: Modifier = Modifier
 ) {
+    val scrollState = rememberScrollState()
     Column(
         modifier = modifier
             .fillMaxSize()
             .background(ShinMunGoTheme.color.gray1)
+            .verticalScroll(scrollState)
     ) {
         HomeTopBar()
-        HomeMileageTitleGroup(
-            mileage = "5,000,000",
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, top = 23.dp)
-        )
 
         Row(
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, top = 15.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 23.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.main_title_my_report),
+                color = ShinMunGoTheme.color.gray13,
+                style = ShinMunGoTheme.typography.body2
+            )
+
+            Text(
+                text = stringResource(R.string.main_title_mileage, mileage),
+                style = ShinMunGoTheme.typography.caption7,
+                color = ShinMunGoTheme.color.gray13
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 15.dp),
             horizontalArrangement = Arrangement.spacedBy(9.dp)
         ) {
             HomeYearReportGroup(
                 title = stringResource(R.string.main_year_report_title),
-                reportCount = "25",
+                reportCount = yearReportCount,
                 reportProgress = 0.7f,
                 replyProgress = 1f,
                 totalProgress = 0f,
-                modifier = Modifier.weight(1f).aspectRatio(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .aspectRatio(1f)
             )
 
             HomeMonthReportGroup(
                 title = stringResource(R.string.main_month_report_title),
-                userName = "수민",
-                reportCount = "2",
-                modifier = Modifier.weight(1f).aspectRatio(1f)
+                userName = userName,
+                reportCount = monthReportCount,
+                modifier = Modifier
+                    .weight(1f)
+                    .aspectRatio(1f)
             )
         }
 
+        HomeImageBannerPager(
+            imageUrls = listOf(
+                "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1731578316860877739.webp",
+                "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1732149068770235113.webp",
+                "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202410/1730192950897967795.webp"
+            ),
+            modifier = Modifier.padding(top = 24.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 25.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.main_example_title),
+                color = ShinMunGoTheme.color.gray13,
+                style = ShinMunGoTheme.typography.body2
+            )
+
+            RoundedCornerTextButton(
+                text = stringResource(R.string.main_example_btn_more),
+                textStyle = ShinMunGoTheme.typography.caption7,
+                textColor = ShinMunGoTheme.color.gray13,
+                backgroundColor = ShinMunGoTheme.color.gray3,
+                roundedCornerShape = RoundedCornerShape(15.dp),
+                onButtonClick = {},
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 5.dp)
+            )
+        }
+
+        HomeIssueExamplePager(
+            imagePairUrls = listOf(
+                Pair(
+                    "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1731578316860877739.webp",
+                    "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1732149068770235113.webp"
+                ),
+                Pair(
+                    "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1732149068770235113.webp",
+                    "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202410/1730192950897967795.webp"
+                ),
+            ),
+            modifier = Modifier.fillMaxWidth().padding(top = 16.dp, bottom = 18.dp)
+        )
     }
 }
 
---
+++ app/src/main/java/com/sopt/shinmungo/presentation/home/component/HomeIssueExamplePager.kt
@@ -0,0 +1,152 @@
+package com.sopt.shinmungo.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.sopt.shinmungo.core.designsystem.component.indicator.PageIndicator
+import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
+
+@Composable
+fun HomeIssueExamplePager(
+    imagePairUrls: List<Pair<String, String>>,
+    modifier: Modifier = Modifier
+) {
+    val pagerState = rememberPagerState { Integer.MAX_VALUE }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            pageSpacing = 16.dp
+        ) { page ->
+            val currentImagePair = imagePairUrls[page % imagePairUrls.size]
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(9.dp)
+            ) {
+                val imageModifier = Modifier
+                    .weight(1f)
+                    .aspectRatio(160 / 105f)
+                HomeIssueExampleImage(
+                    imageUrl = currentImagePair.first,
+                    chipText = "전",
+                    modifier = imageModifier
+                )
+                HomeIssueExampleImage(
+                    imageUrl = currentImagePair.second,
+                    chipText = "후",
+                    modifier = imageModifier
+                )
+            }
+        }
+
+        PageIndicator(
+            pagerState = pagerState,
+            imageCount = imagePairUrls.size,
+            backgroundColor = ShinMunGoTheme.color.gray1,
+            selectedColor = ShinMunGoTheme.color.gray13,
+            unselectedColor = ShinMunGoTheme.color.gray3,
+            modifier = Modifier
+                .padding(top = 4.dp)
+        )
+    }
+}
+
+@Composable
+private fun HomeIssueExampleImage(
+    imageUrl: String,
+    chipText: String,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+
+    Card(
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 1.dp),
+        shape = RoundedCornerShape(15.dp),
+        modifier = modifier
+    ) {
+        Box {
+            AsyncImage(
+                model = ImageRequest.Builder(context)
+                    .data(imageUrl)
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+
+            Box(
+                Modifier
+                    .padding(top = 8.dp, start = 8.dp)
+                    .background(
+                        color = ShinMunGoTheme.color.gray6,
+                        shape = CircleShape
+                    )
+                    .size(16.dp)
+                    .align(Alignment.TopStart)
+            ) {
+                Text(
+                    text = chipText,
+                    style = ShinMunGoTheme.typography.caption8,
+                    color = ShinMunGoTheme.color.gray1,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeIssueExamplePreview() {
+    ShinMunGoTheme {
+        Column {
+            HomeIssueExamplePager(
+                imagePairUrls = listOf(
+                    Pair(
+                        "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1731578316860877739.webp",
+                        "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1732149068770235113.webp"
+                    ),
+                    Pair(
+                        "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1732149068770235113.webp",
+                        "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202410/1730192950897967795.webp"
+                    ),
+                    Pair(
+                        "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202410/1730192950897967795.webp",
+                        "https://image.wavve.com/v1/thumbnails/2480_1396_20_80/meta/image/202411/1731578316860877739.webp"
+                    )
+                ),
+                modifier = Modifier
+            )
+        }
+    }
+}
