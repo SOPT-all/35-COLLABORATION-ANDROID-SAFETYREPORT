cb283491f8e1076fecb9671063b34ccadea1e494
chrin05 <rinring105@gmail.com>
2024-11-24 15:23:47 +0900

    [UI/#25] 신고페이지 내에 카테고리,사진,지역 섹션 추가

--- app/src/main/java/com/sopt/shinmungo/presentation/report/ReportScreen.kt
+++ app/src/main/java/com/sopt/shinmungo/presentation/report/ReportScreen.kt
@@ -1,39 +1,31 @@
 package com.sopt.shinmungo.presentation.report
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.shinmungo.R
-import com.sopt.shinmungo.core.designsystem.component.button.TextWithInfoIcon
 import com.sopt.shinmungo.core.designsystem.component.topbar.CommonTopBar
 import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
 import com.sopt.shinmungo.presentation.report.component.DropdownCategory
+import com.sopt.shinmungo.presentation.report.component.ReportLocationSection
+import com.sopt.shinmungo.presentation.report.component.ReportPhotoSection
 
 @Composable
 fun ReportScreen(
     modifier: Modifier = Modifier
 ) {
+    val viewModel: ReportViewModel = ReportViewModel()
+
     Column(
         modifier = modifier.fillMaxSize().background(color = ShinMunGoTheme.color.gray1)
     ) {
@@ -53,6 +45,22 @@ fun ReportScreen(
                 }
             },
         )
+
+        DropdownCategory(
+            viewModel = viewModel
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(28.dp),
+            modifier = Modifier
+                .padding(top = 18.dp)
+                .padding(horizontal = 18.dp),
+        ) {
+            ReportPhotoSection(viewModel = viewModel)
+
+            ReportLocationSection(viewModel = viewModel)
+            
+        }
     }
 }
 
