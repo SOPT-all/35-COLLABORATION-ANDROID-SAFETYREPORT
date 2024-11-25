d06a88969336348a7b6d1f0aae770f1edb7d91ba
jangsjw <wkdwodnjs1999@gmail.com>
2024-11-25 21:31:48 +0900

    [MOD/#27] TopBar 수정

--- app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryTopBar.kt
+++ app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryTopBar.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
