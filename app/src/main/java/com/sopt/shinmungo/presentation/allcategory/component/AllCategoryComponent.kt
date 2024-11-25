8a919e9ea356c4a736e9b761c05c181ad99f339d
jangsjw <wkdwodnjs1999@gmail.com>
2024-11-25 21:24:19 +0900

    [MOD/#27] AllCategoryComponent 수정

--- app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryComponent.kt
+++ app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryComponent.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sopt.shinmungo.R
 import com.sopt.shinmungo.core.designsystem.component.button.RoundedCornerTextButton
 import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
