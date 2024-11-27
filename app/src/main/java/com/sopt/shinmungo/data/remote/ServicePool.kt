283cf2637004ed58b8e434717f6fa08dc9a0d745
boiledegg <tjrwns2163@naver.com>
2024-11-28 01:59:11 +0900

    [FEAT/#42] Service 구현

--- app/src/main/java/com/sopt/shinmungo/data/remote/ServicePool.kt
+++ app/src/main/java/com/sopt/shinmungo/data/remote/ServicePool.kt
@@ -1,10 +1,15 @@
 package com.sopt.shinmungo.data.remote
 
 import com.sopt.shinmungo.data.service.ExampleService
+import com.sopt.shinmungo.data.service.HomeService
 
 object ServicePool {
     /**TODO: Retrofit 객체 생성 예시 (삭제 예정)*/
     val exampleService by lazy {
         ApiFactory.create<ExampleService>()
+    }
+
+    val homeService by lazy {
+        ApiFactory.create<HomeService>()
     }
 }
