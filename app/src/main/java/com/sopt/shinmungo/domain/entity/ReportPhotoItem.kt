a7e044f8fb1ff7c2f811a04b5c38c2a9f1dc8900
chrin05 <rinring105@gmail.com>
2024-11-28 13:56:33 +0900

    [FIX/#40] serializable 어노테이션 추가

--- app/src/main/java/com/sopt/shinmungo/domain/entity/ReportPhotoItem.kt
+++ app/src/main/java/com/sopt/shinmungo/domain/entity/ReportPhotoItem.kt
@@ -1,6 +1,12 @@
 package com.sopt.shinmungo.domain.entity
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ReportPhotoItem(
+    @SerialName("photoId")
     val photoId: Int,
+    @SerialName("photoUrl")
     val photoUrl: String,
 )
