ce3275cbc9b9260c107bba183fce9bd42ac61860
jangsjw <wkdwodnjs1999@gmail.com>
2024-11-28 20:03:05 +0900

    [FIX/#48] 탑바 LeftIcon Padding처리 수정

--- app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryTopBar.kt
+++ app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryTopBar.kt
@@ -2,7 +2,6 @@ package com.sopt.shinmungo.presentation.allcategory.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -10,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.sopt.shinmungo.R
 import com.sopt.shinmungo.core.designsystem.component.topbar.CommonTopBar
 import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
