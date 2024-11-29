b5c31b7defa326c59a10fc2e2618729a7dc23be5
yihwanggeun <63749140+yihwanggeun@users.noreply.github.com>
2024-11-29 22:28:05 +0900

    Revert "[FIX/#55] 탑바 스타일 문제 해결"

--- app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryTopBar.kt
+++ app/src/main/java/com/sopt/shinmungo/presentation/allcategory/component/AllCategoryTopBar.kt
@@ -2,7 +2,6 @@ package com.sopt.shinmungo.presentation.allcategory.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -10,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.sopt.shinmungo.R
 import com.sopt.shinmungo.core.designsystem.component.topbar.CommonTopBar
 import com.sopt.shinmungo.core.designsystem.theme.ShinMunGoTheme
@@ -23,9 +21,7 @@ fun AllCategoryTopBar(
         CommonTopBar(
             title = stringResource(id = R.string.all_category_title),
             onLeftContent = {
-                IconButton(
-                    modifier = Modifier.size(24.dp),
-                    onClick = onNavigateBack) {
+                IconButton(onClick = onNavigateBack) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_arrow_left_line_white_24),
                         contentDescription = stringResource(id = R.string.back_button_description),
